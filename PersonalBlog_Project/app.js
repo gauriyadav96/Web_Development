a8ad10c57fdb237d928c67daee780a90198301bb
Gauri Yadav <34069023+gauriyadav96@users.noreply.github.com>
2019-08-15 08:50:44 -0400

    uploading failed folders

---
+++ PersonalBlog_Project/app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+var _ = require('lodash'); // for url cases
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function(req,res){
+  res.render("home",{homecontent :homeStartingContent, newPosts: posts});
+});
+
+app.get("/about", function(req,res){
+  res.render("about", {aboutcontent : aboutContent});
+});
+
+app.get("/contact", function(req,res){
+  res.render("contact", {contactcontent : contactContent});
+});
+
+app.get("/compose", function(req,res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req,res){
+
+  const postSection = {
+    title : req.body.newTitle,
+    description : req.body.newPost
+  };
+  posts.push(postSection);
+  res.redirect("/");
+});
+
+// to go to the respective post which was published on the main page
+app.get("/posts/:pname", function(req,res){
+
+const requestedTitle = _.lowerCase(req.params.pname);
+
+    posts.forEach(function(post){
+      let existingTitle = _.lowerCase(post.title);
+          if (existingTitle === requestedTitle){
+            res.render("post", {
+              title:post.title,
+              content:post.description
+            });
+          }
+        });
+
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
